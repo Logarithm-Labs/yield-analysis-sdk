3971d0e20813fc5b0be9438019bb26028f6bd918
TalCrypto <leehunter936@gmail.com>
2025-11-10 16:37:22 +0800

    chore: bump version

--- yield_analysis_sdk/__init__.py
+++ yield_analysis_sdk/__init__.py
@@ -4,7 +4,7 @@ Yield Analysis SDK
 A Python SDK for analyzing DeFi vault performance and yield metrics.
 """
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 __author__ = "Logarithm Labs"
 __email__ = "dev@logarithm.fi"
 
